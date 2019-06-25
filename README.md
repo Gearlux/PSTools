07430837744bbe05a9492e8ee4621694a496dfda
Gert Behiels <gert.behiels@gmail.com>
2019-06-25 17:33:59 +0200

    Conda/Pip functionality

--- README.md
+++ README.md
@@ -15,5 +15,22 @@ $ git clone --recurse-submodules <your repo>
 $ git submodule update --remote
 ```
 
+# Mixing Conda and Pip packages
+
+I find it easy to use conda packages to use eg. tensorflow since it also installs cuda/cudnn libraries. Because conda does not include all packages, I have create the condatools module.
+
+You can call 
+```
+Import-Module condatools.psm1
+CondaInstall gym
+```
+to install OpenAI's gym with pip while most dependencies of gym will be installed with conda.
+
+Similar call
+```
+CondaRequirements
+```
+to install a list of requirements with conda.
+
 # Related
 https://github.com/Liquidmantis/PSCondaEnvs
---
+++ condatools.psm1
@@ -0,0 +1,32 @@
+﻿Function CondaInstall {
+    Param([parameter(position=1)]$Pattern,
+          [parameter(position=2)]$Package="")
+    if ($Package -eq "")
+    {
+      $Package = $Pattern
+    }
+    if ( -Not (&pip list | Select-String -Pattern $Pattern -Quiet) )
+    {
+        Write-Host $Package "conda install"
+        conda install $Package -y
+        if ($LASTEXITCODE -eq 1)
+        {
+            Write-Host $Pattern "failed installation with conda"
+            &pip install $Package --no-deps
+            $requires = &pip show $Package | Select-String Requires
+            $deps = $requires.ToString().Split(':')[1].Split(',')
+            foreach ($dep in $deps) {
+                Write-Host $dep
+                CondaInstall $dep.Trim()
+            }
+        }
+    }
+}
+
+Function CondaRequirements {
+    Param([parameter(position=1)]$requirements="requirements.txt")
+    foreach ($req in (Get-Content $requirements)) {
+        Write-Host $req
+        CondaInstall $req
+    }
+}
